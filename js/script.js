36926f6e7cb6479b39a6d8505359739229ec3613
Paula <pfr.blackcat@gmail.com>
2019-10-15 10:47:00 -0300

    formulario completo, porem não em todas as classes

--- js/script.js
+++ js/script.js
@@ -2,54 +2,60 @@ $(function(){
 
       
 
-        $("#eventType").change(function(){
-            let selected = $("#eventType :selected").val();
+    $("#eventType").change(function(){
+        let selected = $("#eventType :selected").val();
 
 
-            if (selected === "Teatro") {
-                $('.esconder-teatro').hide();
-                $('.mostrar-teatro').show();         
-                
-                
-            }  if (selected === "Cinema") {
-                $('.esconder-cinema').hide();  
-                $('.mostrar-cinema').show(); 
-                   
-            }  if (selected === "Debate Literário") {
-                $('.esconder-debate').hide();  
-                $('.mostrar-debate').show(); 
-
-            }  if (selected === "Dança") {
-                $('.esconder-danca').hide();  
-                $('.mostrar-danca').show(); 
-
-            }  if (selected === 'Artes visuais e exposições'){
-                $('.esconder-artes').hide();  
-                $('.mostrar-artes').show();
-
-            }  if (selected === 'Música'){
-                $('.esconder-musica').hide();  
-                $('.mostrar-musica').show();
-                
-            } if (selected === '-- Selecione --'){
-                $('.esconder-teatro, .mostrar-teatro, .esconder-cinema, .mostrar-cinema, .esconder-debate, .mostrar-debate, .esconder-danca, .mostrar-danca, .esconder-artes, .mostrar-artes, .esconder-musica, .mostrar-musica ').hide();
-            }           
-       
-
-        }); 
-
-
-        
-
-        $('#free').click(function () {
-            $('#price').toggle()
-        })  
-        
-        
-        $('.btn').submit(function () {
+        if (selected === "Teatro") {
+            $('.esconder-teatro').hide();
+            $('.mostrar-teatro').show();         
             
-            if ( )
             
-        })
+        }  if (selected === "Cinema") {
+            $('.esconder-cinema').hide();  
+            $('.mostrar-cinema').show(); 
+               
+        }  if (selected === "Debate Literário") {
+            $('.esconder-debate').hide();  
+            $('.mostrar-debate').show(); 
+
+        }  if (selected === "Dança") {
+            $('.esconder-danca').hide();  
+            $('.mostrar-danca').show(); 
+
+        }  if (selected === 'Artes visuais e exposições'){
+            $('.esconder-artes').hide();  
+            $('.mostrar-artes').show();
+
+        }  if (selected === 'Música'){
+            $('.esconder-musica').hide();  
+            $('.mostrar-musica').show();
+            
+        } if (selected === '-- Selecione --'){
+            $('.esconder-teatro, .mostrar-teatro, .esconder-cinema, .mostrar-cinema, .esconder-debate, .mostrar-debate, .esconder-danca, .mostrar-danca, .esconder-artes, .mostrar-artes, .esconder-musica, .mostrar-musica ').hide();
+      }    
+    }); 
+    
+
+    $('#free').click(function () {
+        $('.valor-entrada').toggle()
+    })  
+    
+    
+    $('form').submit(function (e) {        
+        let valid = true;
+        if ($('#title').is(':visible')&& $('title').val()==''){
+            $('#title').css('border', '1px solid red');
+            $('#title').prop('placeholder', 'digite um texto');
+            valid = false;
+        }
+
+
+        if ( valid == false){
+            e.preventDefault();
+        }
+        
+    })
 
 });
+
