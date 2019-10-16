ebf01e6dc61ad95230a1ddc02c3a9b39a33129f7
DaianaLugoCarvalho <daianalugo.carvalho@gmail.com>
2019-10-16 14:49:03 -0300

    "exercicio do Traversing"

--- transversing/js/page1.js
+++ transversing/js/page1.js
@@ -1,19 +1,22 @@
+// **1.** No arquivo page1.html, faça uma instrução jQuery que selecione todos elementos img da página que possuem alt e os imprima no log do console.
+
 $(function(){
 
     // $(document).ready(function(){
     //     $("img").parent('.alt');
-    // });
-
-
-    // $(document).ready(function(){
-    //     $("img").attr("alt");
-    // })
+    // });  
 
 
     $(document).ready(function(){
-        $("img").siblings("alt");
+        $("img").attr("alt");
     });
     
 })
 
+
+
+
+
+       
+        
 
